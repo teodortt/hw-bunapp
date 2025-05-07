eb546f74a0a5ea05844c219297f1ee560575bff8
teodortt <t.todorov@jdm.degit>
2025-05-07 11:00:10 +0300

    Refactor TabLayout component and add Settings screen with external link functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,37 @@
-import {Button, Text} from '@/components/ui';
-import {List} from '@/lib/icons/List';
-import {Settings} from '@/lib/icons/Settings';
+import { Button, Text } from "@/components/ui";
+import { List } from "@/lib/icons/List";
+import { BookOpen } from "@/lib/icons/Book";
+import { Settings } from "@/lib/icons/Settings";
 
-import {Tabs} from 'expo-router';
-import {Pressable} from 'react-native';
+import { Tabs } from "expo-router";
+import { Store } from "lucide-react-native";
+import { Pressable } from "react-native";
 export const unstable_settings = {
   initialRouteName: "index",
 };
 
 export default function TabLayout() {
   return (
-    <Tabs >
+    <Tabs>
+      <Tabs.Screen
+        name="offers"
+        options={{
+          title: "Offers",
+          tabBarIcon: ({ color }) => <BookOpen className="text-foreground" />,
+        }}
+      />
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Habits',
-          tabBarIcon: ({color}) => <List className="text-foreground" />,
+          title: "Habits",
+          tabBarIcon: ({ color }) => <List className="text-foreground" />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
-          tabBarIcon: ({color}) => <Settings className="text-foreground" />,
+          title: "Settings",
+          tabBarIcon: ({ color }) => <Settings className="text-foreground" />,
         }}
       />
     </Tabs>
