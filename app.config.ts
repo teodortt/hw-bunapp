9b697f8a1233a8ad7639573e202da95623260741
teodortt <t.todorov@jdm.degit>
2025-11-16 15:24:17 +0200

    fix: update versionCode in app.json and app.config.ts to 41

--- app.config.ts
+++ app.config.ts
@@ -33,7 +33,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#ffffff",
     },
     package: "com.happyworld.base",
-    googleServicesFile: "./google-services.json",
+    versionCode: 41,
   },
   web: {
     bundler: "metro",
